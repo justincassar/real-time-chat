491134aad703bf29f9a15906cdad89c7d2cb0bc1
Justin Cassar <justcassar@gmail.com>
2021-06-29 22:22:58 +0200

    Created HTML files

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,20 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
 const socket = io();
+
+let userName = "";
+
+const newUserConn = (user) => {
+    userName = user;
+    socket.emit("new user", userName);
+    addToUsers(userName);
+};
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
 
 socket.on('message', message => {
     console.log(message);
@@ -21,4 +37,17 @@ function outputMessage(message) {
     div.classList.add('message');
     div.innerHTML = `<p>${message}</p>`;
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+function addToUsers(userName){
+    if (!!document.querySelector(`.${userName}-userlist`)) {
+        return;
+    }
+
+    const userBox = `
+        <div class="chat_ib ${userName}-userlist">
+          <h5>${userName}</h5>
+        </div>
+      `;
+    inboxPeople.innerHTML += userBox;
 }
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const server = require('http').createServer(app)
 const io = require('socket.io')(server);
+
+// Set port
 const PORT = 8080;
 
+// Create array for connections
+const connections = [];
+
+// Set static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Server started on port
@@ -13,18 +19,27 @@ server.listen(PORT, () => console.log('App listening on port', server.address().
 io.on('connection', socket => {
     console.log('Client connected');
 
+    socket.on("new user", (data) => {
+        socket.userId = data;
+        connections.add(data);
+        io.emit("new user", [...connections]);
+    })
+
     // Send message to user
     socket.emit('message', 'Connection to chat client established')
 
     // Broadcast message to all connections except the user
     socket.broadcast.emit('message', 'New user connected to the chat');
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+
 
     // Send user chat to all connected clients
     socket.on('chatMsg', (msg) => {
         io.emit('message', msg);
     })
+
+    socket.on('disconnect', () => {
+        connections.delete(socket.userId);
+        console.log('Client disconnected');
+    });
 });
