723474a5efbaf012973e5f262be0f856d9306848
Justin Cassar <justcassar@gmail.com>
2021-07-05 22:25:21 +0200

    Functional public chat between clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,53 +1,50 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-
 const socket = io();
 
-let userName = "";
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('user-list');
+const messageForm = document.getElementById('message-form');
+const msgInput = document.getElementById('msg-input');
+var userName = document.getElementById('username');
+
+userName = "";
 
 const newUserConn = (user) => {
     userName = user;
-    socket.emit("new user", userName);
+    socket.emit('user-join', userName);
     addToUsers(userName);
 };
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });
-
-socket.on('message', message => {
+// Message from server
+socket.on('message', (message) => {
     console.log(message);
-    outputMessage(message);
+    showMessage(message);
 });
 
-chatForm.addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
 
     // Forms usually submit to a file so it can be stopped by using preventDefault();
     e.preventDefault();
 
-    const msg = e.target.elements.msg.value;
+    const msg = msgInput.value;
 
     // Send user message to all chat clients
-    socket.emit('chatMsg', msg);
+    socket.emit('chat-msg', msg);
+    msgInput.value = '';
 });
 
-function outputMessage(message) {
+function showMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
     div.innerHTML = `<p>${message}</p>`;
     document.querySelector('.chat-messages').appendChild(div);
-}
+};
 
-function addToUsers(userName){
-    if (!!document.querySelector(`.${userName}-userlist`)) {
-        return;
-    }
-
-    const userBox = `
-        <div class="chat_ib ${userName}-userlist">
-          <h5>${userName}</h5>
-        </div>
-      `;
-    inboxPeople.innerHTML += userBox;
-}
+function showUsers(users) {
+    userList.innerText = '';
+    users.forEach((user) => {
+        const li = document.createElement('li');
+        li.innerText = user.displayname;
+        userList.appendChild(li);
+    });
+};
--- server.js
+++ server.js
@@ -8,38 +8,37 @@ const io = require('socket.io')(server);
 const PORT = 8080;
 
 // Create array for connections
-const connections = [];
+const users = [];
 
 // Set static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Server started on port
-server.listen(PORT, () => console.log('App listening on port', server.address().port));
+server.listen(PORT, () => console.log('Listening on port', server.address().port));
 
 io.on('connection', socket => {
-    console.log('Client connected');
 
-    socket.on("new user", (data) => {
-        socket.userId = data;
-        connections.add(data);
-        io.emit("new user", [...connections]);
-    })
-
-    // Send message to user
-    socket.emit('message', 'Connection to chat client established')
+    console.log(`${socket.id} connected`);
 
     // Broadcast message to all connections except the user
-    socket.broadcast.emit('message', 'New user connected to the chat');
+    socket.on('user-join', (name) => {
+        users.push({ id: socket.id, name })
+        console.log(name);
+        socket.broadcast.emit(`${name} connected`)
+    });
 
-
+    // socket.on('typing', (data) => {
+    //     socket.broadcast.emit('typing', { username: socket.username });
+    // });
 
     // Send user chat to all connected clients
-    socket.on('chatMsg', (msg) => {
-        io.emit('message', msg);
-    })
+    socket.on('chat-msg', (data) => {
+        console.log(data);
+        io.emit('message', data)
+    });
 
     socket.on('disconnect', () => {
-        connections.delete(socket.userId);
         console.log('Client disconnected');
+        delete users[socket.userId];
     });
 });
